63d69f8934b6f348aa79b705ea8b08d256514289
Ali Yağız Uz <aliyagizuz@gmail.com>
2025-05-29 21:15:39 +0300

    i2c'ler ayarlandı

--- stm/StarDustV2/Core/Inc/main.h
+++ stm/StarDustV2/Core/Inc/main.h
@@ -57,6 +57,14 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define BHI_I2C_SCL_Pin GPIO_PIN_10
+#define BHI_I2C_SCL_GPIO_Port GPIOB
+#define BMP_I2C_SCL_Pin GPIO_PIN_8
+#define BMP_I2C_SCL_GPIO_Port GPIOA
+#define BHI_I2C_SDA_Pin GPIO_PIN_3
+#define BHI_I2C_SDA_GPIO_Port GPIOB
+#define BMP_I2C_SDA_Pin GPIO_PIN_4
+#define BMP_I2C_SDA_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
--- stm/StarDustV2/Core/Src/main.c
+++ stm/StarDustV2/Core/Src/main.c
@@ -18,6 +18,8 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "i2c.h"
+#include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -84,6 +86,9 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_I2C2_Init();
+  MX_I2C3_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
