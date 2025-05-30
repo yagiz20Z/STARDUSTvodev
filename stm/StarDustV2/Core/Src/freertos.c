6adefd9248960281b582c35f45e232432d71e3b6
zeynepoztrk <nochuct08@gmail.com>
2025-05-30 20:14:42 +0300

    include

--- stm/StarDustV2/Core/Src/freertos.c
+++ stm/StarDustV2/Core/Src/freertos.c
@@ -22,6 +22,7 @@
 #include "task.h"
 #include "main.h"
 #include "cmsis_os.h"
+#include "stdio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
